68caa09129aaa88afe2994d7121a5210743f657d
Nortover <staspushko7@gmail.com>
2025-03-12 11:01:08 +0200

    Update Trapeze.cpp

--- Lab-1.3-OOP-cpp/Trapeze.cpp
+++ Lab-1.3-OOP-cpp/Trapeze.cpp
@@ -2,25 +2,25 @@
 #include <cmath> 
 #include <iostream>
 
-// Конструктор за замовчуванням 
+// ГЉГ®Г­Г±ГІГ°ГіГЄГІГ®Г° Г§Г  Г§Г Г¬Г®ГўГ·ГіГўГ Г­Г­ГїГ¬ 
 Trapeze::Trapeze()
     : x1(0), y1(0), x2(0), y2(0), x3(0), y3(0), x4(0), y4(0)
 {
 }
 
-// Конструктор з параметрами
+// ГЉГ®Г­Г±ГІГ°ГіГЄГІГ®Г° Г§ ГЇГ Г°Г Г¬ГҐГІГ°Г Г¬ГЁ
 Trapeze::Trapeze(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
     : x1(x1), y1(y1), x2(x2), y2(y2), x3(x3), y3(y3), x4(x4), y4(y4)
 {
 }
 
-// Конструктор копії
+// ГЉГ®Г­Г±ГІГ°ГіГЄГІГ®Г° ГЄГ®ГЇВіВї
 Trapeze::Trapeze(const Trapeze& other)
     : x1(other.x1), y1(other.y1), x2(other.x2), y2(other.y2), x3(other.x3), y3(other.y3), x4(other.x4), y4(other.y4)
 {
 }
 
-// Делегування конструктора
+// Г„ГҐГ«ГҐГЈГіГўГ Г­Г­Гї ГЄГ®Г­Г±ГІГ°ГіГЄГІГ®Г°Г 
 Trapeze::Trapeze(double x1, double y1) : Trapeze(x1, y1, 0, 0, 0, 0, 0, 0)
 {
 }
@@ -29,7 +29,7 @@ Trapeze::~Trapeze() {
 
 }
 
-// Сетери
+// Г‘ГҐГІГҐГ°ГЁ
 void Trapeze::setCoordinates(double X1, double Y1, double X2, double Y2, double X3, double Y3, double X4, double Y4)
 {
     x1 = X1;
@@ -43,7 +43,7 @@ void Trapeze::setCoordinates(double X1, double Y1, double X2, double Y2, double 
 
 }
 
-// Перевантаження: встановлення координат через масив з 8 елементів
+// ГЏГҐГ°ГҐГўГ Г­ГІГ Г¦ГҐГ­Г­Гї: ГўГ±ГІГ Г­Г®ГўГ«ГҐГ­Г­Гї ГЄГ®Г®Г°Г¤ГЁГ­Г ГІ Г·ГҐГ°ГҐГ§ Г¬Г Г±ГЁГў Г§ 8 ГҐГ«ГҐГ¬ГҐГ­ГІВіГў
 void Trapeze::setCoordinates(const double coords[8]) {
     x1 = coords[0]; y1 = coords[1];
     x2 = coords[2]; y2 = coords[3];
@@ -51,7 +51,7 @@ void Trapeze::setCoordinates(const double coords[8]) {
     x4 = coords[6]; y4 = coords[7];
 }
 
-// Гетери
+// ГѓГҐГІГҐГ°ГЁ
 double Trapeze::getX1() const { return x1; }
 double Trapeze::getY1() const { return y1; }
 double Trapeze::getX2() const { return x2; }
@@ -88,8 +88,44 @@ double Trapeze::getArea() const
 
 }
 
+void Trapeze::scale(double factor)
+{
+    x1 *= factor;
+    y1 *= factor;
+    x2 *= factor;
+    y2 *= factor;
+    x3 *= factor;
+    y3 *= factor;
+    x4 *= factor;
+    y4 *= factor;
 
-// Перевантаження оператора * для множення двох трапецій
+}
+
+Trapeze Trapeze::scale(double factor, Trapeze other)
+{
+    other.x1 *= factor;
+    other.y1 *= factor;
+    other.x2 *= factor;
+    other.y2 *= factor;
+    other.x3 *= factor;
+    other.y3 *= factor;
+    other.x4 *= factor;
+    other.y4 *= factor;
+    return other;
+}
+
+void Trapeze::scale(Trapeze& other, double factor)
+{
+    other.x1 *= factor;
+    other.y1 *= factor;
+    other.x2 *= factor;
+    other.y2 *= factor;
+    other.x3 *= factor;
+    other.y3 *= factor;
+}
+
+
+// ГЏГҐГ°ГҐГўГ Г­ГІГ Г¦ГҐГ­Г­Гї Г®ГЇГҐГ°Г ГІГ®Г°Г  * Г¤Г«Гї Г¬Г­Г®Г¦ГҐГ­Г­Гї Г¤ГўГ®Гµ ГІГ°Г ГЇГҐГ¶ВіГ©
 Trapeze Trapeze::operator*(const Trapeze& other) const {
     return Trapeze(x1 * other.x1, y1 * other.y1,
         x2 * other.x2, y2 * other.y2,
@@ -98,7 +134,7 @@ Trapeze Trapeze::operator*(const Trapeze& other) const {
     }
 
 
-// Перевантаження оператора вирахування
+// ГЏГҐГ°ГҐГўГ Г­ГІГ Г¦ГҐГ­Г­Гї Г®ГЇГҐГ°Г ГІГ®Г°Г  ГўГЁГ°Г ГµГіГўГ Г­Г­Гї
 Trapeze Trapeze::operator-(double subtract) const {
     return Trapeze(
         x1 - subtract, y1 - subtract,
@@ -119,7 +155,7 @@ Trapeze& Trapeze::operator+=(const Trapeze& other) {
     y4 += other.y4;
     return *this;
 }
-    // Перевантаження оператора виведення
+    // ГЏГҐГ°ГҐГўГ Г­ГІГ Г¦ГҐГ­Г­Гї Г®ГЇГҐГ°Г ГІГ®Г°Г  ГўГЁГўГҐГ¤ГҐГ­Г­Гї
     std::ostream& operator<<(std::ostream & os, const Trapeze & trapeze) {
         os << "Trapeze(" << trapeze.x1 << ", " << trapeze.y1 << ", " << trapeze.x2 << ", " << trapeze.y2 << ", " << trapeze.x3 << ", " << trapeze.y3 << ", " << trapeze.x4 << ", " << trapeze.y4 << ")";
         return os;
